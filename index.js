905176c8c5d9d5ff165d4d7259af638a6dbe2df4
Tilinn <jm293229@gmail.com>
2024-12-01 08:20:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ const rl = createInterface(process.stdin, process.stdout)
 //const app = express()
 //const port = process.env.PORT || 8080;
 
-say('PIKACHU\nBot', {
+say('Stich\nBot', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Si eres tilín, te paso a recordar lo gay que eres xD`, {
+say(`creado por tilin ventas `, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
